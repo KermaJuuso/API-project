7f5aeeba2e4ef0d658f4d8e8a8a864886434b644
Juuso <juusotoivanen03@gmail.com>
2025-01-16 11:54:07 +0200

    Add simple api request to get puuid

--- profileApp/riot_api.py
+++ profileApp/riot_api.py
@@ -1,3 +1,18 @@
 from dotenv import load_dotenv
 import os
 import requests
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+
+def get_puuid(region, gameName, tagLine):
+    api_url = (f"https://{region}.api.riotgames.com/riot/"
+               f"account/v1/accounts/by-riot-id/{gameName}/{tagLine}")
+    api_url = api_url + '?api_key=' + API_KEY
+
+    info = requests.get(api_url)
+    info = info.json()
+    print(info)
+
+get_puuid("europe", "ukko2000", "EUW")
