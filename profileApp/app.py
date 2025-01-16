988245e6e73b05433d8dfc7f6a947932c22c2d2a
Juuso <juusotoivanen03@gmail.com>
2025-01-16 15:43:08 +0200

    add route to /profile

--- profileApp/app.py
+++ profileApp/app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+from riot_api import get_puuid
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "This will be my app!"
+def index():
+    """Homepage"""
+    return render_template('index.html')
+
+@app.route('/profile', methods=['POST'])
+def profile():
+    game_name = request.form.get('gameName')
+    tag_line = request.form.get('tagLine')
+    region = request.form.get('region')
+
+    print(f"Game Name: {game_name}, Tag Line: {tag_line}, Region: {region}")
+
+    try:
+        puuid = get_puuid(region, game_name, tag_line)
+        return render_template('profile.html', puuid=puuid, gameName=game_name,
+                               tagLine=tag_line)
+    except Exception as e:
+        return render_template('error.html', message=str(e))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- profileApp/riot_api.py
+++ profileApp/riot_api.py
@@ -7,12 +7,21 @@ API_KEY = os.getenv("API_KEY")
 
 
 def get_puuid(region, gameName, tagLine):
+    """
+    Return the puuid of player. Puuid is used to get
+    the stats of the player.
+    :param region: str, EUROPE, AMERICAS, ASIA, ESPORTS
+    :param gameName:str, ingame name of player
+    :param tagLine:str, 3-5 letters
+    :return: str, puuid
+    """
     api_url = (f"https://{region}.api.riotgames.com/riot/"
                f"account/v1/accounts/by-riot-id/{gameName}/{tagLine}")
     api_url = api_url + '?api_key=' + API_KEY
 
-    info = requests.get(api_url)
-    info = info.json()
-    print(info)
-
-get_puuid("europe", "ukko2000", "EUW")
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        info = response.json()
+        return info['puuid']
+    else:
+        raise Exception(f"Error: {response.status_code}, {response.json()}")
