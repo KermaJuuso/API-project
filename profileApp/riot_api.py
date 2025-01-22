7a72b8f7ee772f13a8cbab51aad0d7ee97d88161
Juuso <juusotoivanen03@gmail.com>
2025-01-22 17:55:00 +0200

    Add get_match and MatchData obj.

--- profileApp/riot_api.py
+++ profileApp/riot_api.py
@@ -5,15 +5,39 @@ import requests
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
+#Remove this later
+PUUID = os.getenv("MY_PUUID")
+
+
+class MatchData:
+    def __init__(self, metadata, info, puuid):
+        self.metadata = metadata
+        self.info = info
+
+        # Find all the user-specific data
+        self.my_id = puuid
+        self.my_index = self.metadata['participants'].index(self.my_id)
+        self.my_data = info['participants'][self.my_index]
+
+    @property
+    def did_i_win(self):
+        return self.my_data['win']
+
+    @property
+    def get_champion(self):
+        return self.my_data['championName']
+
+
 
 def get_puuid(region, gameName, tagLine):
     """
     Return the puuid of player. Puuid is used to get
     the stats of the player.
+    ACCOUNT-V1
     :param region: str, EUROPE, AMERICAS, ASIA, ESPORTS
     :param gameName:str, ingame name of player
     :param tagLine:str, 3-5 letters
-    :return: str, puuid
+    :return: dict
     """
     api_url = (f"https://{region}.api.riotgames.com/riot/"
                f"account/v1/accounts/by-riot-id/{gameName}/{tagLine}")
@@ -22,27 +46,67 @@ def get_puuid(region, gameName, tagLine):
     response = requests.get(api_url)
     if response.status_code == 200:
         info = response.json()
-        return info['puuid']
+        puuid = info['puuid']
+        name = info['gameName']
+        tag_line = info['tagLine']
+        return {
+            "puuid": puuid,
+            "name": name,
+            "tagLine": tag_line,
+        }
     else:
         raise Exception(f"Error: {response.status_code}, {response.json()}")
 
 
-def get_summoner_info():
+def get_summoner_info(server, puuid):
     """
     This functions purpose is to fetch, summoner level and icon
     SUMMONER-V4
-    :return:
+    :return: dict
     """
-    return
+
+    api_url = (f"https://{server}.api.riotgames.com/lol/summoner/v4/"
+               f"summoners/by-puuid/{puuid}?api_key={API_KEY}")
+
+    response = requests.get(api_url)
+    info = response.json()
+    profile_icon = info['profileIconId']
+    summoner_level = info['summonerLevel']
+
+    return {
+        "profileIconId": profile_icon,
+        "summonerLevel": summoner_level,
+    }
 
 
-def get_match_history():
+def get_matches(region, puuid):
     """
-    This function fetches match information
+    Gets user's 20 most recent game IDs and turns them into MatchData objects.
     MATCH-V5
-    :return:
+    :param region: str, region of the user
+    :param puuid: str, players ID
+    :return: list of MatchData objects for user match history.
     """
-    return
+    #Get match ids
+    api_url = (f"https://{region}.api.riotgames.com/lol/match/v5/matches/"
+               f"by-puuid/{puuid}/ids?start=0&count=20&api_key={API_KEY}")
+
+    response = requests.get(api_url)
+    #Get list of game ids
+    ids = response.json()
+
+    match_list = []
+    for match in ids:
+        #Get match data
+        api_url = (f"https://{region}.api.riotgames.com/lol/match/v5/matches/"
+                   f"{match}?api_key={API_KEY}")
+
+        response = requests.get(api_url)
+        data = response.json()
+        match_obj = MatchData(data['metadata'], data['info'], puuid)
+        match_list.append(match_obj)
+
+    return match_list
 
 
 def get_champion_mastery():
@@ -62,5 +126,10 @@ def is_in_game_currently():
     """
     return
 
+
 def get_player_stats(region, gameName, tagLine):
     return
+
+
+list = get_matches('europe', PUUID)
+print(list[0].did_i_win)
